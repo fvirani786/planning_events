71940064717a66d7bb6e2b86fcd251b53ecfcfe6
Farid Virani <fvirani786@gmail.com>
2024-08-10 11:57:18 -0500

    feat:delete

--- events/models.py
+++ events/models.py
@@ -1,4 +1,4 @@
-from django.db import models 
+from django.db import models
 
 class Event(models.Model):
     title = models.CharField(max_length=200)
@@ -16,6 +16,7 @@ class Participant(models.Model):
 
     def __str__(self):
         return self.name
+
 
 
 # Create your models here.
--- events/views.py
+++ events/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from .models import Event
+from .models import Event, Participant
 from .forms import EventForm
 
 # List all events
@@ -37,4 +37,15 @@ def event_delete(request, pk):
         event.delete()
         return redirect('event_list')
     return render(request, 'event_confirm_delete.html', {'event': event})
+
+# Add a participant to an event
+def add_participant(request, event_id):
+    event = get_object_or_404(Event, id=event_id)
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        Participant.objects.create(event=event, name=name, email=email)
+        return redirect('event_detail', event_id=event.id)
+    return render(request, 'add_participant.html', {'event': event})
+
 
